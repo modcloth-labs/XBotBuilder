27c91e35b115501c0ddb5a3a3d6778f73f708cde
Nick Rowe <nixterrimus@dcxn.com>
2014-10-08 22:17:45 -0700

    Persist Github Config and restore

--- XBotBuilder/XBotBuilder/GithubConfig.swift
+++ XBotBuilder/XBotBuilder/GithubConfig.swift
@@ -9,26 +9,41 @@
 import Foundation
 
 class GithubConfig {
+    var defaults : NSUserDefaults!
+    
     init(){
-        
+        self.defaults = NSUserDefaults.standardUserDefaults()
     }
     
     var apiToken: String {
         get {
-            return "Super Long String of numbers and letters"
+            return fetchFromDefaults("apiToken")
         }
-        set {
-            
+        set(value) {
+            persistToDefaults("apiToken", value:value)
         }
     }
     
     var projectIdentifier: String {
         get {
-            return "modcloth-lab/xbot"
+            return fetchFromDefaults("projectIdentifier")
         }
-        set {
-            
+        set(value) {
+            persistToDefaults("projectIdentifier", value:value)
         }
     }
-
+    
+    func namespacedKey(key:String) -> String{
+        var id = 1
+        return "github/\(id)/\(key)"
+    }
+    
+    func fetchFromDefaults(key:String) -> String! {
+        return self.defaults.objectForKey(namespacedKey(key)) as AnyObject! as NSString! ?? ""
+    }
+    
+    func persistToDefaults(key:String, value:String) {
+        self.defaults.setObject(value, forKey: namespacedKey(key))
+        self.defaults.synchronize()
+    }
 }
